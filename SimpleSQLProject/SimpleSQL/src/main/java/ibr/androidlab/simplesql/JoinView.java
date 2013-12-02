78ba2c2c43c13f331c58d620adc581a07bfa9e3d
Felix Geilert <root92@live.de>
2013-12-02 22:07:51 +0100

    added basic shell for activity class

---
+++ SimpleSQLProject/SimpleSQL/src/main/java/ibr/androidlab/simplesql/JoinView.java
@@ -0,0 +1,40 @@
+package ibr.androidlab.simplesql;
+
+/**
+ * 
+ */
+public class JoinView extends Activity {
+
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+
+    /**
+     * Add class to the join list
+     * 
+     */
+    private addTable() {
+
+    }
+
+    /**
+     * Change the position of the given table handle
+     *
+     */
+    private changeTablePos() {
+
+
+    }
+
+    /**
+     * Remove the given table from the list
+     *
+     */
+    private removeTable() {
+
+
+    }
+}
