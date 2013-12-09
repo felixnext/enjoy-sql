d601d45df420ab771d9b2f9c84659af7da7c651d
Felix Geilert <root92@live.de>
2013-12-09 14:59:44 +0100

    loaded story in join view / created singleton to provide access to the story

---
+++ SimpleSQLProject/SimpleSQL/src/main/java/utilities/views/list/ListFragment.java
@@ -0,0 +1,17 @@
+package utilities.views.list;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Felix on 09.12.13.
+ */
+public class ListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+}
