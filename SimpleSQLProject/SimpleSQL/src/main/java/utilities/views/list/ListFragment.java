ba4355ca519384a3da9a3a80dffd2696299c9a40
Felix Geilert <root92@live.de>
2013-12-10 20:58:04 +0100

    used list fragment in the view / added some small layout classes for table data / added some listener (still not imlemented) / NOTE: in my android studio still problems with R class

--- SimpleSQLProject/SimpleSQL/src/main/java/utilities/views/list/ListFragment.java
+++ SimpleSQLProject/SimpleSQL/src/main/java/utilities/views/list/ListFragment.java
@@ -12,6 +12,9 @@ import android.view.ViewGroup;
 import android.app.Activity;
 import android.widget.ArrayAdapter;
 
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
 import ibr.androidlab.simplesql.Database.SharedData;
 import ibr.androidlab.simplesql.R;
 import ibr.androidlab.simplesql.xmlLoader.data.Table;
@@ -22,26 +25,54 @@ import utilities.views.table.TableFragment;
  */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class ListFragment extends Fragment {
+
+    /**
+     * Create Listener for tables as anonymous functions
+     */
+    private OnItemClickListener mMainItemClickedHandler = new OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            //TODO: start drag and drop events here
+        }
+    };
+
+    /**
+     * Create Listener for tables as anonymous functions
+     */
+    private OnItemClickListener mSideItemClickedHandler = new OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            //TODO: start drag and drop events here
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //process: bind the fragment layout to the code file
-        return inflater.inflate(ibr.androidlab.simplesql.R.layout.list_fragement, container, false);
+        return inflater.inflate(R.layout.list_fragement, container, false);
 
-
+        //process: bind the tables
+        bindTables();
     }
 
     /**
 	 * Binds the given table array to the interface fragment
 	*/
-    public void bindTables(Table[] tables) {
+    public void bindTables() {
     	//TODO attach tables to this fragment
         //process: create the adapter using the shared data component for all tables
         ArrayAdapter adapter = new ArrayAdapter<Table>(this, R.drawable.big_join_table, SharedData.getInstance().currentStory.tables);
         ListView listView = (ListView) getView().findViewById(R.id.joinMainList);
         listView.setAdapter(adapter);
 
-        //process: create the adapter using the shared data component for the joined tables
+        //process: add the click listener to the mainJoinList
+        listView.setOnItemClickListener(mMainItemClickedHandler);
 
+        //process: create the adapter using the shared data component for the joined tables
+        adapter = new ArrayAdapter<String>(this, R.drawable.small_join_table, SharedData.getInstance().getJoinTablesArray());
+        listView = (ListView) getView().findViewById(R.id.joinSideList);
+        listView.setAdapter(adapter);
+
+        //process: add the click listener to the sideJoinList
+        listView.setOnItemClickListener(mSideItemClickedHandler);
     }
 
     @Override
