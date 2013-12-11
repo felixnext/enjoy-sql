89a4c2979f2d94a474b256b9e56cd5c9d30814af
yevgen <y.pikus@tu-bs.de>
2013-12-11 16:33:13 +0100

    update

--- SimpleSQLProject/SimpleSQL/src/main/java/ibr/androidlab/simplesql/ConditionsView.java
+++ SimpleSQLProject/SimpleSQL/src/main/java/ibr/androidlab/simplesql/ConditionsView.java
@@ -1,8 +1,11 @@
 package ibr.androidlab.simplesql;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import utilities.views.cond.ColumnsFragment;
 
@@ -14,6 +17,14 @@ public class ConditionsView extends Activity implements ColumnsFragment.Selected
     final public static String SELECTED_COLUMNS = "sected_columns";
 
     private String[] selectedColumns;
+
+    //spinner with selected items
+    private ColumnsFragment columnSpinner1  = (ColumnsFragment) getFragmentManager().findFragmentById(R.id.columns_spinner1);
+    private ColumnsFragment columnSpinner2  = (ColumnsFragment) getFragmentManager().findFragmentById(R.id.columns_spinner2);
+    private ColumnsFragment columnSpinner3  = (ColumnsFragment) getFragmentManager().findFragmentById(R.id.columns_spinner3);
+    private ColumnsFragment columnSpinner4  = (ColumnsFragment) getFragmentManager().findFragmentById(R.id.columns_spinner4);
+    private ColumnsFragment columnSpinner5  = (ColumnsFragment) getFragmentManager().findFragmentById(R.id.columns_spinner5);
+
 
 
     @Override
@@ -27,7 +38,19 @@ public class ConditionsView extends Activity implements ColumnsFragment.Selected
             selectedColumns = intent.getStringArrayExtra(SELECTED_COLUMNS);
         }
 
+        createOperatorsSpinner();
 
+        
+
+    }
+
+    private void createOperatorsSpinner() {
+        Spinner sp = (Spinner) findViewById(R.id.operator);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operators, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+        sp.setPrompt("Operator");
+        sp.setSelection(0);
 
     }
 
