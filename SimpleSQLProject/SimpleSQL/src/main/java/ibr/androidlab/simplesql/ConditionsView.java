a567cbf83835dfae84e0dc6de5c3913c55d5387d
yevgen <y.pikus@tu-bs.de>
2013-12-11 12:30:22 +0100

    intent handling added

---
+++ SimpleSQLProject/SimpleSQL/src/main/java/ibr/androidlab/simplesql/ConditionsView.java
@@ -0,0 +1,35 @@
+package ibr.androidlab.simplesql;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import utilities.views.cond.ColumnsFragment;
+
+/**
+ * Created by yevgen on 10.12.13.
+ */
+public class ConditionsView extends Activity implements ColumnsFragment.SelectedColumnsDeliverer {
+
+    final public static String SELCTED_COLUMNS = "sected_columns";
+
+    private String[] selectedColumns;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comdition_view);
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            selectedColumns = intent.getStringArrayExtra(SELCTED_COLUMNS);
+        }
+
+    }
+
+    @Override
+    public String[] getSectedColumns() {
+        return selectedColumns;
+    }
+}
